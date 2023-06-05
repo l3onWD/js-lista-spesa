67702f0e7d1afecbbe9760199b23180e87517267
l3on <l3onwebdev@gmail.com>
2023-06-05 15:12:19 +0200

    added steps file

---
+++ docs/steps.md
@@ -0,0 +1,11 @@
+# Lista della spesa
+
+<br>
+
+## Steps:
+
+- Creare una lista di prodotti da acquistare
+- **FINCHE** _"ho elementi nella lista"_
+    - Prendo il prodotto corrente
+    - Inserisco l'elemento nella lista di stampa
+- Stampo la lista a schermo
--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-/* -----------------------------------------
-! DEBUG
--------------------------------------------*/
-console.log('----------- DEBUG -----------');
+/*
+Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
-console.log('JS OK!');// Check if JS is linked to the page
-
+Consigli:
+    -Facciamo un primo giro col for, in modo da sentirci confident sul risultato della stampa, poi trasformiamo il for in un while
+    - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+    - Ricordiamoci di incrementare il contatore all'interno ciclo while per evitare di cascare in un loop infinito.
+*/
 
 /* -----------------------------------------
 * INIT
